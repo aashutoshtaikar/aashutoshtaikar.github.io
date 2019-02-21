1406903a400e120394254da39ad507a948aba89c
aashutoshtaikar <aashutoshtaikar@gmail.com>
2019-02-21 01:49:19 -0800

    new

---
+++ metahead.js
@@ -0,0 +1,7 @@
+
+document.write("<meta charset=\"utf-8\">");
+document.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+document.write("<link rel=\"stylesheet\" href=\"https:\/\/fonts.googleapis.com\/icon?family=Material+Icons\">");
+document.write("<link rel='stylesheet' href='https:\/\/use.fontawesome.com\/releases\/v5.7.0\/css\/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>");
+document.write("");
+
