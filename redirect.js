fe121cc07c64eb7117110bd2957f790acf89c542
Aashutosh <aashutoshtaikar@gmail.com>
2019-06-03 19:50:48 -0400

    test3

--- redirect.js
+++
@@ -1,5 +0,0 @@
-function redirectToMypage(params) {
-    if (window.location.href==="https://aashutoshtaikar.github.io") {
-        window.location.href = "https://sites.google.com/view/aashutoshtaikar/home"; 
-    }    
-}
