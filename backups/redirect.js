9213dad2b46f8ea1f75ba6edb5653530bedf96f8
Aashutosh Taikar <aashutoshtaikar@gmail.com>
2019-07-29 17:43:14 -0400

    upated

---
+++ backups/redirect.js
@@ -0,0 +1,5 @@
+function redirectToMypage() {
+    // if (window.location.href==="https://aashutoshtaikar.github.io") {
+        window.location.href = "https://sites.google.com/view/aashutoshtaikar/home"; 
+    // }    
+}
