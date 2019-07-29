6a5e30bb997afdaae528f4e7480a10f9bb24ef3a
Aashutosh Taikar <aashutoshtaikar@gmail.com>
2019-07-29 17:37:28 -0400

    Delete redirect.js

--- redirect.js
+++
@@ -1,5 +0,0 @@
-// function redirectToMypage() {
-//     // if (window.location.href==="https://aashutoshtaikar.github.io") {
-//         window.location.href = "https://sites.google.com/view/aashutoshtaikar/home"; 
-//     // }    
-// }
